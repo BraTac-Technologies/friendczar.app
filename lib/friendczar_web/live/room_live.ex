f094106c9d0d90b33b272941a16e012cb4dbfbec
Yasen Kostov <ykostov@pm.me>
2022-04-08 12:44:15 +0300

    enroll sending messages

--- lib/friendczar_web/live/room_live.ex
+++ lib/friendczar_web/live/room_live.ex
@@ -4,6 +4,8 @@ defmodule FriendczarWeb.RoomLive do
   alias FriendczarWeb.Repo
   alias FriendczarWeb.ChatView
   alias Friendczar.Accounts
+  alias Friendczar.Messages
+  alias Friendczar.Messages.Message
 
 
   def render(assigns) do
@@ -15,15 +17,34 @@ defmodule FriendczarWeb.RoomLive do
     selected_user = Accounts.get_user!(params["id"])
     current_user_token = session["user_token"]
     current_user = Accounts.get_user_by_session_token(current_user_token)
-    IO.inspect(current_user, label: "HERE===>")
+    changeset_for_message = Messages.change_message(%Message{})
     socket = assign(
         socket,
         selected_user: selected_user,
-        current_user: current_user
+        current_user: current_user,
+        changeset_for_message: changeset_for_message
       )
     {:ok, socket}
   end
 
 
+  def handle_event("send_message", %{"message" => attrs}, socket) do
+
+    case Messages.create_message(attrs) do
+      {:ok, message} ->
+
+        changeset_for_message = Messages.change_message(%Message{})
+        socket = assign(socket, changeset_for_message: changeset_for_message)
+        {:noreply, socket}
+
+      {:error, %Ecto.Changeset{} = changeset_for_message} ->
+
+        socket = assign(socket, changeset_for_message: changeset_for_message)
+        {:noreply, socket}
+
+      end
+
+
+  end
 
 end
